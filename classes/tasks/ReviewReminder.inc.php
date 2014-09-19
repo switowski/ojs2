9f0a21bb0432dff95b2df927293134deed08703d
Sebastian Witowski <sebastian.witowski@cern.ch>
2014-09-19 15:18:41 +0200

    Multiple automatic review reminders
    * Review reminders will now be send more than once (the interval has to
      be hard-coded in ReviewReminder.inc.php file).

--- classes/tasks/ReviewReminder.inc.php
+++ classes/tasks/ReviewReminder.inc.php
@@ -141,8 +141,12 @@ class ReviewReminder extends ScheduledTask {
 				}
 			}
 
-			if ($reviewAssignment->getDateReminded() !== null) {
-				$shouldRemind = false;
+			// After the first reminder we want to keep sending notifications
+			// in a predefined interval (so reviewers won't forget to review the submission)
+			$notificationInterval = 60 * 60 * 24 * 7 // We want to send reminders once per week
+			$remindedDate = strtotime($reviewAssignment->getDateReminded());
+			if (time() - $remindedDate > $notificationInterval ) {
+				$shouldRemind = true;
 			}
 
 			if ($shouldRemind) $this->sendReminder ($reviewAssignment, $article, $journal);
