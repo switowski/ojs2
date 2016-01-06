ec266f4f459dd6b5ae06c39706dbd4f390201748
Sebastian Witowski <witowski.sebastian@gmail.com>
2016-01-06 16:08:55 +0100

    Changed one TODO into NOTE comment

--- tools/ojs_tasks.py
+++ tools/ojs_tasks.py
@@ -81,7 +81,7 @@ Subject: {subject}
 def author_reminder():
     """Checks if it is time to remind author about copyediting and sends an email"""
 
-    # TODO Interval here doesn't matter - the interval is setup by the CRON job, if we run the job once per week, then the email is send once per week
+    # NOTE Interval here doesn't matter - the interval is setup by the CRON job, if we run the job once per week, then the email is send once per week
     # you should change it in case the cronjob is run everyday and you want the emails to be send less often
     AR_NOTIFICATION_INTERVAL = datetime.timedelta(days=1)
     AR_EMAIL_TITLE = "Copyediting Review Reminder"
